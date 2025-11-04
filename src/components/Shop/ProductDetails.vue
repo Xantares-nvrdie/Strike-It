71170e18ac3c3298c251fca56413e8461d4f2b79
Bintang Fajar Putra Pamungkas <bintangfajarputra@gmail.com>
2025-11-04 10:44:17 +0700

    add component product-details

--- src/components/Shop/ProductDetails.vue
+++ src/components/Shop/ProductDetails.vue
@@ -1,16 +1,39 @@
 <script setup>
+    import { ref } from 'vue'
+    import { Icon } from '@iconify/vue';
+    const isAvailable = ref(true)
 
+    const thumbnailImages = ref([
+        'images/poster1.png',
+        'images/poster1.png',
+        'images/poster1.png',
+        'images/poster1.png',
+        'images/poster1.png',
+        'images/poster1.png',
+    ]);
+    
+    const mainImage = ref(thumbnailImages.value[0]);
+    function selectThumbnail(image) {
+        mainImage.value = image;
+    }
 </script>
 
 <template>
     <section class="w-full min-h-screen p-5 flex flex-col justify-start items-start bg-zinc-100">
-        <div class="w-full p-5 bg-gray-500 flex flex-row justify-center items-center gap-10 rounded-lg">
-            <div class="flex-1 bg-red-200 p-5 rounded-lg flex flex-col justify-center">
-                <img src="../../assets/poster1.png" alt="Product" class="max-w-[513px] max-h-[497px] w-full object-cover mx-auto">
-                <div class="flex flex-row justify-center items-center mt-10 gap-20">
-                    <img src="../../assets/poster1.png" alt="Product" class="w-[100px] h-[100px] object-cover">
-                    <img src="../../assets/poster1.png" alt="Product" class="w-[100px] h-[100px] object-cover">
-                    <img src="../../assets/poster1.png" alt="Product" class="w-[100px] h-[100px] object-cover">
+        <div class="w-full p-5 bg-zinc-100 flex flex-row justify-center items-center gap-10 rounded-lg">
+            <div class="flex-1 bg-zinc-100 shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)] p-5 rounded-lg flex flex-col justify-center">
+                <img :src="mainImage" alt="Product" class="max-w-[513px] max-h-[497px] w-full object-cover mx-auto rounded-lg">
+                
+                <div class="flex flex-row items-center mt-10 gap-4 shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)] overflow-x-auto whitespace-nowrap px-4 py-2 bg-zinc-100">
+                    <img 
+                        v-for="(image, index) in thumbnailImages" 
+                        :key="index"
+                        :src="image" 
+                        :alt="'Thumbnail ' + (index + 1)" 
+                        class="w-[100px] h-[100px] object-cover rounded-lg cursor-pointer flex-shrink-0
+                            border-2 transition-all duration-200
+                            border-transparent hover:border-blue-600" :class="{'border-blue-600': image === mainImage}" @click="selectThumbnail(image)"
+                    >
                 </div>
             </div>
             <div class="flex-1 bg-blue-200 p-5 rounded-lg text-black">
@@ -22,7 +45,18 @@
                     <li>Spesifikasi Joran</li>
                 </ul>
                 <div class="mt-2.5">
-                    In Stock?
+                    <p v-if="isAvailable" class="inline-flex items-center gap-2">
+                        <Icon icon="tabler:checkbox" width="24" height="24" class="text-green-400"/>
+                        <span class="text-black text-lg font-normal font-outfit leading-8">
+                            In stock
+                        </span>
+                    </p>
+                    <p v-else class="inline-flex items-center gap-2">
+                        <Icon icon="healthicons:no-outline-24px" width="24" height="24" class="text-red-400"/>
+                        <span class="text-gray-500 text-lg font-normal font-outfit leading-8">
+                            Stok Habis
+                        </span>
+                    </p>
                 </div>
                 <div class="flex flex-row gap-4 mt-4">
                     <input 
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 import ProductDetails from '@/components/Shop/ProductDetails.vue';
 import ShopHeader from '@/components/Shop/ShopHeader.vue';
+import ProductDetailsDescription from '@/components/Shop/ProductDetailsDescription.vue';
 </script>
 
 
@@ -9,6 +10,9 @@ import ShopHeader from '@/components/Shop/ShopHeader.vue';
     <div class="relative w-full min-h-screen overflow-x-hidden bg-zinc-100">
         <ShopHeader class="w-full sticky top-[65px] z-40 bg-zinc-100 py-4 block" />
         <ProductDetails class="mt-20"/>
+        <div class="p-5 md:p-10">
+            <ProductDetailsDescription class="mt-10 mb-20 px-5 md:px-20"/>
+        </div>
     </div>
 </template>
 
